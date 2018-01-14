7070ef462b57a40555664d658557deebbf409567
Kenneth Tso <iamkennytso@gmail.com>
2018-01-14 18:42:48 -0500

    (fit) webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-var path = require('path');
 var webpack = require('webpack');
 module.exports = {
   entry: './client/index.jsx',
-  output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js'
-  },
+  output: './publc/bundle.js',
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/,                query: {
-                    presets: ['react','es2015']
-                } }
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,                
+        query: {
+          presets: ['react']
+        }
+       }
     ],
   }
 }
